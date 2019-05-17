590909b995bcf4d5bac97848139b898a8cb833e6
Kishore Jagadeesan <kjj28@bath.ac.uk>
2019-05-17 11:24:49 +0100

    initial commit

---
+++ inst/shiny-Apps/pda/ui.R
@@ -0,0 +1,27 @@
+
+# CSS to use in the app
+appCSS <-
+  ".mandatory_star { color: red; }
+.shiny-input-container { margin-top: 25px; }
+#submit_msg { margin-left: 15px; }
+#error { color: red; }
+body { background: #fcfcfc; }
+#header { background: #fff; border-bottom: 1px solid #ddd; margin: -20px -15px 0; padding: 15px 15px 10px; }
+"
+
+
+ui <- dashboardPage(
+  dashboardHeader(title = 'PDA 1.0'),
+  dashboardSidebar(
+    shinyjs::useShinyjs(),
+    shinyjs::inlineCSS(appCSS),
+    tags$head(includeScript("google-analytics.js")),
+    uiOutput("sidebarpanel")
+  ),
+
+dashboardBody(
+  uiOutput("body")
+  )
+
+
+)
