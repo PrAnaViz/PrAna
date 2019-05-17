f772ec4a08eead4eaf2910a328041a2cb7a83358
Kishore Jagadeesan <kjj28@bath.ac.uk>
2019-05-17 11:31:44 +0100

    initial commit

---
+++ R/runShiny.R
@@ -0,0 +1,36 @@
+
+#' Run in-built Shiny Apps in the package
+#'
+#' @param appName In-built Shiny App name. Example: plotFlow
+#'
+#' @return Opens up the in-built Shiny App.
+#' @export
+#'
+#' @examples
+#' runShiny('pda')
+#'
+#' stpdata::runShiny('pda')
+#'
+runShiny <- function(appName) {
+  # locate all the shiny app examples that exist
+  validExamples <- list.files(system.file("shiny-Apps", package = "PRANA"))
+
+  validExamplesMsg <-
+    paste0(
+      "Valid in-built apps are: '",
+      paste(validExamples, collapse = "', '"),
+      "'")
+
+  # if an invalid example is given, throw an error
+  if (missing(appName) || !nzchar(appName) ||
+      !appName %in% validExamples) {
+    stop(
+      'Please run `runShiny()` with a valid in-built app as an argument.\n',
+      validExamplesMsg,
+      call. = FALSE)
+  }
+
+  # find and launch the app
+  appDir <- system.file("shiny-Apps", appName, package = "PRANA")
+  shiny::runApp(appDir, display.mode = "normal")
+}
