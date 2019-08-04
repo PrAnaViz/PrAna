eced04acb3f2aa80d5ea5344a8d88187e60839c5
Kishore Jagadeesan <kjj28@bath.ac.uk>
2019-08-04 13:40:35 +0100

    tab timeseries updated

--- inst/shiny-Apps/pda_2/server.R
+++ inst/shiny-Apps/pda_2/server.R
@@ -7,6 +7,8 @@
 #    http://shiny.rstudio.com/
 #
 
+#library(shinydashboard)
+#source(file.path("ui"))
 options(shiny.maxRequestSize = 5000000*1024^6)
 
 ## Login details
@@ -215,46 +217,9 @@ server <- function(input, output, session) {
           
           
           tabBox(width = 10,height = 1500,
-                 tabPanel('Time series - Bar Plot Complete',
-                          br(),
-                          
-                          column(9,
-                                 box(width = "1300px",
-                                     br(),
-                                     # downloadButton ('downdat.time_barplot1',label = "Yearwise"),
-                                     # downloadButton ('downdat.data_api_monthwise_full2',label = "Monthwise"),
-                                     plotlyOutput("timeseries_bar_plot1",height="600px",width = "1000px"))
-                                 
-                                 
-                          ),
-                          column(12,
-                                 tabBox(width = NULL,
-                                        tabPanel("Month wise - with Average",value = "filt_tab_year_01",
-                                                 br(),
-                                                 downloadButton ('downdat.data_time_comp04',label = "Selected API"),
-                                                 downloadButton ('downdat.data_time_comp03',label = "Complete"),
-                                                 plotlyOutput("timeseries_line_comp_plot1",height="550px")
-                                        ),
-                                        #               #             tabPanel("Data Table",
-                                        #               #                     dataTableOutput("tab_data_time_comp07")
-                                        #               #                     ),
-                                        #               # tabPanel("Data Table 2",
-                                        #               #          dataTableOutput("tab_data_time_comp07_02")
-                                        #               # ),
-                                        tabPanel("Month wise",value = "filt_tab_year_02",
-                                                 br(),
-                                                 # downloadButton ('downdat.data_time_comp04',label = "Selected API"),
-                                                 # downloadButton ('downdat.data_time_comp03',label = "Complete"),
-                                                 # plotlyOutput("timeseries_line_comp_plot1",height="550px")
-                                                 plotlyOutput("timeseries_line_plot4",height="450px")
-                                        )
-                                        
-                                        
-                                        
-                                        
-                                 )
-                          )# End of column
-                 )
+                 # include the UI for each tab
+                 source(file.path("ui", "tab-timeseries.R"),  local = TRUE)$value
+                 
                  
           ) # End of tabBox     
         ) # End of fluidRow
