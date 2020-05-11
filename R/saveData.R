ef45533c2083066f4b6929ae64f93888997b30e3
jkkishore85 <jkkishore85@gmail.com>
2020-05-11 15:50:40 +0100

    modified saveData return

--- R/saveData.R
+++ R/saveData.R
@@ -4,17 +4,23 @@
 #'  
 #' @param data csv data
 #' @param tab_name Table name in the tbe DB
+#' @param selectdb enter the interested db
+#' @param usernm input your MySQL username
+#' @param pwd input your MySQL password
 #'
-#' @return
+#' @return a table in MySQL
 #' @export
 #'
-saveData <-function (data,tab_name,selectdb){
+saveData <-function (data,tab_name,selectdb, usernm,pwd){
+  
+  dw <- config::get("datawarehouse")
+  
   con <-  DBI::dbConnect (
     drv = RMariaDB::MariaDB(),
-    username ='kjsql', 
-    password = 'nEd01@fd7TZl',
-    dbname =  selectdb ,
-    host='localhost'
+    username = usernm, 
+    password = pwd,
+    host='localhost',
+    dbname =  selectdb 
     )
   on.exit(DBI::dbDisconnect(con), add = TRUE)
   DBI::dbWriteTable(con,tab_name,data,row.names= FALSE,  append=T)
