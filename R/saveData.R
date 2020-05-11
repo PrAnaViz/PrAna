56e17f8a786e9b10abee1182f193b0fd237b306e
jkkishore85 <jkkishore85@gmail.com>
2020-05-11 16:57:43 +0100

    usrnm, pwd, dbname added

--- R/saveData.R
+++ R/saveData.R
@@ -13,14 +13,12 @@
 #'
 saveData <-function (data,tab_name,selectdb, usernm,pwd){
   
-  dw <- config::get("datawarehouse")
-  
-  con <-  DBI::dbConnect (
-    drv = RMariaDB::MariaDB(),
-    username = usernm, 
-    password = pwd,
-    host='localhost',
-    dbname =  selectdb 
+    con <-  DBI::dbConnect (
+          drv = RMariaDB::MariaDB(),
+          username = usernm, 
+          password = pwd,
+          host='localhost',
+          dbname =  selectdb 
     )
   on.exit(DBI::dbDisconnect(con), add = TRUE)
   DBI::dbWriteTable(con,tab_name,data,row.names= FALSE,  append=T)
