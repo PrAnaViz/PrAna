32e58cbf90674725c6e240d59e91c1924a9185d2
jkkishore85 <jkkishore85@gmail.com>
2020-05-11 14:00:24 +0100

    DBI functions updated

--- R/saveData.R
+++ R/saveData.R
@@ -9,14 +9,14 @@
 #' @export
 #'
 saveData <-function (data,tab_name,selectdb){
-  con <- dbConnect (
+  con <-  DBI::dbConnect (
     drv = RMariaDB::MariaDB(),
     username ='kjsql', 
     password = 'nEd01@fd7TZl',
-    dbname =selectdb ,
+    dbname =  selectdb ,
     host='localhost'
     )
-  on.exit(dbDisconnect(con), add = TRUE)
-  dbWriteTable(con,tab_name,data,row.names= FALSE,  append=T)
-  dbDisconnect(con)
+  on.exit(DBI::dbDisconnect(con), add = TRUE)
+  DBI::dbWriteTable(con,tab_name,data,row.names= FALSE,  append=T)
+  DBI::dbDisconnect(con)
 }  
