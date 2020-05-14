37b470d4fd694187155ccc36711a407cfbb6bf57
Kishore Jagadeesan <kjj28@bath.ac.uk>
2020-05-14 01:23:13 +0100

    uk_practice_wise function added

---
+++ R/uk_practice_wise.R
@@ -0,0 +1,67 @@
+#' Title
+#'
+#' @param x 
+#' @param y 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+uk_practice_wise <- function(x,y) {
+  
+  tab01 <- NULL
+  tab02 <- NULL
+  tab03 <- NULL
+  tab04 <- NULL
+  s <- NULL
+  tab1 <- NULL
+  tab2 <- NULL
+  tab3 <- NULL
+  
+  for (i in (1:length( unique(y[[2]])  )))
+  {
+    
+    j = unique(y[[2]])[i]
+    
+    tab01 <- x %>%
+      filter (PRACTICE %in% j) %>%
+      data.table::setnames(4,"BNFCODE") %>%
+      merge(bnf_full_final_02,by = "BNFCODE", all = F) %>%
+      select("PERIOD","PRACTICE","BNFCODE","MDR","VPPID","VPID.x","ISID","value","UOM","ITEMS","QUANTITY")
+    
+    s <- strsplit(tab01$ISID, split = "#")
+    
+    tab02<- data.frame(
+      PERIOD = rep(tab01$'PERIOD', sapply(s, length)),
+      PRACTICE = rep(tab01$'PRACTICE', sapply(s, length)),
+      BNF_CODE = rep(tab01$'BNFCODE', sapply(s, length)),
+      VPPID = rep(tab01$'VPPID', sapply(s, length)),
+      VPID = rep(tab01$'VPID.x', sapply(s, length)),
+      MDR = rep(tab01$'MDR', sapply(s, length)),
+      ITEMS = rep(tab01$'ITEMS', sapply(s, length)),
+      QUANTITY = rep(tab01$'QUANTITY', sapply(s, length)),
+      API = unlist(strsplit(tab01$'ISID', split ="#")),
+      Vol_Mass =  sapply(unlist(strsplit(tab01$'value', split ="#")), as.numeric),
+      UOM = unlist(strsplit(tab01$'UOM', split = "#"))
+    )
+    tab03 <- cbind(tab02,Vol_Mass_Tot = (tab02$'Vol_Mass'*tab02$'QUANTITY')* (uom_2$'multi_fac'[match(gsub(" ", "",tab02$'UOM'), gsub(" ", "",  uom_2$'CD'))]))
+    tab04 <- cbind(tab03, DForm = dform$'FORMCD'[match(gsub(" ", "",tab03$'VPID'), gsub(" ", "",  dform$'VPID'))])
+    
+    tab1  <- tab04 %>%
+      dplyr::select("PERIOD", "PRACTICE","API","Vol_Mass_Tot","DForm")%>%
+      mutate(API= as.character(API))%>%
+      mutate(API_CODE = API) %>%
+      group_by(API_CODE,PRACTICE,PERIOD,DForm)%>%
+      dplyr::summarize(gram = sum(Vol_Mass_Tot, na.rm = T)) %>%
+      left_join(ing2[,c(1,5)], by="API_CODE") %>%
+      mutate(gram = gram/1000)%>%
+      select("NM", "API_CODE","PRACTICE","PERIOD","DForm","gram")
+    
+    tab2 <- aggrapi(tab1[, c("NM", "gram")])
+    
+    tab3 <- cbind(CPD = tab2$'.id' [match(gsub(" ", "",tab1$"NM"), gsub(" ", "", tab2$'NM'))],tab1)
+    
+    write.csv(tab3, paste0( j,".csv"))
+    
+  }
+}
