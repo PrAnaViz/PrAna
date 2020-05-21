4c96eb3c67eae2a068bbfcd5f5d0e8880efeb4fe
jkkishore85 <jkkishore85@gmail.com>
2020-05-21 13:30:37 +0100

    added global variables

--- R/globals.R
+++ R/globals.R
@@ -1,5 +1,6 @@
 utils::globalVariables(c("VPID", "summarise", "ISID",
                          "STRNT_NMRTR_VAL","STRNT_NMRTR_UOMCD",".",
                          "API", "API_CODE", "BNFCODE" ,"DForm" ,"PERIOD", 
-                         "PRACTICE", "V1", "Vol_Mass_Tot", "dform", "gram", "ing2", "uom_2"
+                         "PRACTICE", "V1", "Vol_Mass_Tot", "dform", "gram", "ing2", "uom_2",
+                         "FORMCD", "QUANTITY", "UOM", "Vol_Mass", "multi_fac"
 ))
