215b74235f145ef7d97df213e5a3250949f58729
Kishore Jagadeesan <kjj28@bath.ac.uk>
2021-02-15 04:21:40 +0000

    js files removed

--- inst/shiny-Apps/PrAnaViz/ui.R
+++ inst/shiny-Apps/PrAnaViz/ui.R
@@ -1,18 +1,9 @@
-
-
 options(shiny.maxRequestSize = 5000000*1024^6)
-ui <- dashboardPage(
-  
-  #tags$head(HTML("<script type='text/javascript' src='myscript.js'></script>")),
-  header = dashboardHeader(title = 'PrAnaViz'),
-  body = dashboardBody(
-    #tags$head(tags$script(src = "myscript.js")),
-    uiOutput("body")
+ui <- shinydashboard::dashboardPage(
+  header = shinydashboard::dashboardHeader(title = 'PrAnaViz'),
+  body = shinydashboard::dashboardBody(
+   shiny::uiOutput("body")
   ),
-  sidebar = dashboardSidebar(
-    uiOutput("sidebarpanel"))
-  
-  
-  
-  
+  sidebar = shinydashboard::dashboardSidebar(
+    shiny::uiOutput("sidebarpanel"))
 )
