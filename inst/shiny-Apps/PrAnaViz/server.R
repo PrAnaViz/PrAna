320bbfd71fbfa478a27397f5787b3da106720fa2
Kishore Jagadeesan <kjj28@bath.ac.uk>
2021-02-15 04:21:13 +0000

    added mysql input options

--- inst/shiny-Apps/PrAnaViz/server.R
+++ inst/shiny-Apps/PrAnaViz/server.R
@@ -1,102 +1,116 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-# #
-
 source("global.R")
 options(shiny.maxRequestSize = 5000000*1024^6)
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
   
-  sql.demo_one <- "SELECT * FROM demo_one." 
+  sql.demo_one <- "SELECT * FROM gp_practice." 
   sql.support <- "SELECT * FROM support." 
   
-  # output file
+   # Connect to local host 
+  aggr_wodb <- reactive({
+    DBI:: dbConnect(
+      drv = RMariaDB::MariaDB(),
+      username = input$usernm ,
+      password = isolate(input$pwd), 
+      host=input$hostnm)
+  }) 
   
-  # Connect to local host 
-  aggr_wodb <- DBI:: dbConnect(
-    drv = RMariaDB::MariaDB(),
-    username ='jkkis', 
-    password = 'mysqlpassword', 
-    host='localhost')
   
-  dform_desc <-  tbl(aggr_wodb, sql("SELECT * FROM support.dform_desc" ))
+  dform_desc <- reactive({
+    tbl(aggr_wodb(), sql("SELECT * FROM support.dform_desc" ))
+  })  
   
-  gp_list_ccg_c <- tbl(aggr_wodb, sql("SELECT * FROM support.full_gp_ccg_commissioner" ))
-  
-  gp_list_shape <- tbl(aggr_wodb, sql("SELECT * FROM support.full_gp_shapewise" )) %>%
-    mutate (region = toupper(region))
+  gp_list_shape <- reactive({
+    tbl(aggr_wodb(), sql("SELECT * FROM support.full_gp_shapewise" )) %>%
+     mutate (region = toupper(region))
+  })
   
   # select table bnf_group
   tbls_bnf_group <- "bnf_group"
   
   # df from support bnf_group
-  df_bnf_group <-  lapply(paste(sql.support, tbls_bnf_group), function(s) {
-    tryCatch({ return(dbGetQuery(aggr_wodb, s)) 
-    }, error = function(e) return(as.character(e)))
-  })
+  df_bnf_group <- reactive({
+    lapply(paste(sql.support, tbls_bnf_group), function(s) {
+      tryCatch({ return(dbGetQuery(aggr_wodb(), s)) 
+      }, error = function(e) return(as.character(e)))
+    })
+  })  
   
   # bnf group table
-  bnf_group <-   base::do.call(rbind, df_bnf_group) %>%
-    mutate(BNF_CHEMICAL_SUBSTANCE =(tolower(BNF_CHEMICAL_SUBSTANCE)))
+  bnf_group <- reactive({
+    base::do.call(rbind, df_bnf_group()) %>%
+      mutate(BNF_CHEMICAL_SUBSTANCE =(tolower(BNF_CHEMICAL_SUBSTANCE)))
+  })  
   
   # select table dfrom_desc
   tbls_dform_desc<- "dform_desc"
   
   # df from support dfrom_desc
-  df_dform_desc <-  lapply(paste(sql.support, tbls_dform_desc), function(s) {
-    tryCatch({ return(dbGetQuery(aggr_wodb, s)) 
-    }, error = function(e) return(as.character(e)))
-  })
+  df_dform_desc <-  reactive({
+    lapply(paste(sql.support, tbls_dform_desc), function(s) {
+      tryCatch({ return(dbGetQuery(aggr_wodb(), s)) 
+      }, error = function(e) return(as.character(e)))
+    })
+  }) 
   
   # dfrom_desc table
-  dform_desc <-   base::do.call(rbind, df_dform_desc) 
+  dform_desc <- reactive({
+    base::do.call(rbind, df_dform_desc()) 
+  }) 
   
   # select table gp data latnlong
   tbls_gp_data_full_latnlong <- "gp_data_full_latnlong"
   
   # df from support gp data latnlong
-  df_gp_data_full_latnlong <-  lapply(paste(sql.support, tbls_gp_data_full_latnlong), function(s) {
-    tryCatch({ return(dbGetQuery(aggr_wodb, s)) 
-    }, error = function(e) return(as.character(e)))
-  })
+  df_gp_data_full_latnlong <- reactive({
+    lapply(paste(sql.support, tbls_gp_data_full_latnlong), function(s) {
+      tryCatch({ return(dbGetQuery(aggr_wodb(), s)) 
+      }, error = function(e) return(as.character(e)))
+    })
+  }) 
   
   # postcode_full table
-  gp_data_full_latnlong <-   base::do.call(rbind, df_gp_data_full_latnlong)
+  gp_data_full_latnlong <- reactive({
+    base::do.call(rbind, df_gp_data_full_latnlong()) %>%
+      mutate(latitude = as.numeric(latitude),
+             longitude = as.numeric(longitude))
+  })  
   
   # For the UI to show all the regions based on year
-  gp_list_year_tab  <- reactive ({ 
-    gp_list_shape %>%
+  gp_list_year_tab  <- reactive ({
+    gp_list_shape () %>%
     filter (Year %in% !!input$selectyear01) %>%
     select(region)
   })
   
-  ## To see the regions
+  
   output$regioninput1 <- renderUI({
     withProgress(message = 'Data is loading, please wait ...', value = 1:100, {
       selectInput(inputId="region_select_01",
                   label="Select region:",
-                  choices=unique(as.data.frame(gp_list_year_tab() )),selected ="NHS BATH AND NORTH EAST SOMERSET CCG",  multiple = FALSE
+                  choices=map()$region_name
       )
     })
   })
+  
+ 
   
   # select table postcode_full
   tbls_ccg_gp_full <- "ccg_gp_full_latnlong"
   
   # df from support postcode_full
-  df_ccg_gp_full <-  lapply(paste(sql.support, tbls_ccg_gp_full), function(s) {
-    tryCatch({ return(dbGetQuery(aggr_wodb, s)) 
-    }, error = function(e) return(as.character(e)))
-  })
+  df_ccg_gp_full <- reactive({
+    lapply(paste(sql.support, tbls_ccg_gp_full), function(s) {
+      tryCatch({ return(dbGetQuery(aggr_wodb(), s)) 
+      }, error = function(e) return(as.character(e)))
+    })
+  }) 
   
   # postcode_full table
-  ccg_gp_full <-   base::do.call(rbind, df_ccg_gp_full)
+  ccg_gp_full <- reactive({
+    base::do.call(rbind, df_ccg_gp_full())
+  }) 
   
   ## To see the gp setting type
   output$settinginput1 <- renderUI({
@@ -104,7 +118,7 @@ server <- function(input, output, session) {
       selectInput(inputId="setting_select_01",
                   label="Setting type:",
                   choices=c("all" , 
-                            unique( ccg_gp_full$setting_type )),
+                            unique( ccg_gp_full()$setting_type )),
                   selected ="GP Practice",  multiple = FALSE
       )
     })
@@ -119,16 +133,26 @@ server <- function(input, output, session) {
   
   ## Plots
   source(file.path("server", "tab-targeted.R"),  local = TRUE)$value
-  source(file.path("server", "tab-non-targeted.R"),  local = TRUE)$value  
+  source(file.path("server", "tab-non-targeted.R"),  local = TRUE)$value
   
   ## Outputs
   output$sidebarpanel <- renderUI({
       div(
         sidebarMenu(
           tags$hr(),
+          menuItem("Database connect", 
+                   
+                   textInput("usernm", "user name:", "type here"),
+                   passwordInput("pwd", "Password:"),
+                   textInput("hostnm", "host name:", "type here"),
+                   br(),
+                   actionButton("mysql_submit", "Submit")
+                   ),
+          tags$hr(),
           menuItem("Targeted", tabName = "targeted"),
           menuItem("Non-Targeted", tabName = "non-targeted"),
           tags$hr(),
+          uiOutput("uifile_shape"),
           selectInput('selectyear01', 'Select Prescirption Year:',
                       c(
                         '2015' = '2015',
