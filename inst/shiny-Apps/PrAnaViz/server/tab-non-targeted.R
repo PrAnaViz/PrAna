dc1e82222d0bd744db0bf82e45202b5b43292013
Kishore Jagadeesan <kjj28@bath.ac.uk>
2021-02-15 04:27:39 +0000

    shp files input added

--- inst/shiny-Apps/PrAnaViz/server/tab-non-targeted.R
+++ inst/shiny-Apps/PrAnaViz/server/tab-non-targeted.R
@@ -1,3 +1,34 @@
+## Shape file
+
+# ui for file input 01
+output$uifile_shape <- renderUI({
+  tipify(fileInput('file_shape', 'Upload shape files at once (.shp, .dbf, .shx and .prj):',
+                   accept=c('.shp','.dbf','.sbn','.sbx','.shx',".prj"), 
+                   multiple=TRUE,
+                   placeholder = "Upload shape files (.shp, .dbf, .shx and .prj)"
+  ),
+  "Click here to upload your shape files")
+  
+})
+
+map <- reactive({
+  shpdf <- input$file_shape
+  if(is.null(shpdf)){
+    return()
+  }
+  previouswd <- getwd()
+  uploaddirectory <- dirname(shpdf$datapath[1])
+  setwd(uploaddirectory)
+  for(i in 1:nrow(shpdf)){
+    file.rename(shpdf$datapath[i], shpdf$name[i])
+  }
+  setwd(previouswd)
+  map <- sf::st_read(paste(uploaddirectory, shpdf$name[grep(pattern="*.shp$", shpdf$name)], sep="/"), quiet = TRUE ) %>%
+    rename(region_name = 3)
+  map
+})
+
+
 # Leaflet color function
 getColor <- function(data02) {
   sapply(data02$kg, function(kg) {
@@ -13,49 +44,10 @@ getColor <- function(data02) {
 }
 
 gp_postcode_full <-  reactive ({ 
-  gp_data_full_latnlong %>%
+  gp_data_full_latnlong() %>%
     filter (Year %in% !!input$selectyear01)  %>%
     dplyr::select(PERIOD, PRACTICE, SURGERY_NAME, postcode, latitude, longitude) %>%
     distinct()
-})
-
-select_api <-  reactive({	
-  bind_practices () %>%	
-    filter (!grepl("Error: Table",CPD)) %>%	
-    dplyr::mutate(NM = tolower(NM) ) %>% 	
-    dplyr::filter (str_detect(NM , paste(!!bnf_group$BNF_CHEMICAL_SUBSTANCE,collapse = '|'))) %>%	
-    dplyr::rename (BNF_CHEMICAL_SUBSTANCE = NM) %>%	
-    select(BNF_CHEMICAL_SUBSTANCE)	    
-})
-
-
-# Select shape files by user inputs
-shape_select <- reactive ({
-  if (input$selectyear01 == "2015") {
-    a1 <- readOGR("shape_files/CCG_2015/CCG_2015.shp")
-    a2 <- a1[toupper(a1$ccg15nm) == input$region_select_01,]
-    a2
-  }
-  else if (input$selectyear01 == "2016") {
-    a1 <- readOGR("shape_files/CCG_2016/CCG_2016.shp")
-    a2 <- a1[toupper(a1$ccg16nm) ==input$region_select_01,]
-    a2
-  }
-  else if (input$selectyear01 == "2017") {
-    a1 <- readOGR("shape_files/CCG_2017/CCG_2017.shp")
-    a2 <- a1[toupper(a1$ccg17nm) == input$region_select_01,]
-    a2
-  }
-  else if (input$selectyear01 == "2018") {
-    a1 <- readOGR("shape_files/CCG_2018/CCG_2018.shp")
-    a2 <- a1[toupper(a1$ccg18nm) ==  input$region_select_01,]
-    a2
-  }
-}) 
-
-# Transform shape files for leaflet
-sub_region <- reactive({
-  sp::spTransform(shape_select(), "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
 })
 
 # Add zero before the month
@@ -68,14 +60,11 @@ month_sel <- reactive ({
   }
 })
 
-group_postcode <- reactive({
+postcode_api <- reactive({
   bind_practices () %>%
+    mutate(Year=(substr(PERIOD, 1, 4)),gram2 = as.numeric(gram)) %>%
     filter (!grepl("Error: Table",CPD)) %>%
-    dplyr::mutate(NM = tolower(NM) ) %>% 
-    dplyr::filter (str_detect(NM , paste(!!bnf_group$BNF_CHEMICAL_SUBSTANCE,collapse = '|'))) %>%
-    mutate(Year=(substr(PERIOD, 1, 4))) %>%
-    filter (Year %in% !!input$selectyear01) %>%
-    mutate(gram2 = as.numeric(gram)) %>%
+    dplyr::filter ( tolower(NM) %in% !!input$selected_api) %>%
     left_join(gp_postcode_full(), by=c("PRACTICE"="PRACTICE", "PERIOD" = "PERIOD")) %>%
     group_by(SURGERY_NAME, NM, Year, PERIOD, PRACTICE, postcode, latitude, longitude) %>%
     summarise(gram_sum = sum(gram2, na.rm = T)) %>%
@@ -83,265 +72,90 @@ group_postcode <- reactive({
     mutate(month=(substr(PERIOD, 5, 6))) 
 })
 
-postcode_api <- reactive({
-  group_postcode() %>%
-    filter ( tolower(NM) %in% !!input$selected_api) 
-})
-
-postcode_api_month <- reactive ({
-  postcode_api() %>%
-    filter (month %in% month_sel()) 
-})
-
-# coordinates for setview
-sub_coord <- reactive({
-  as.data.frame( sp::coordinates(sub_region()))
-})
-
-# leaflet icons
-icons <- reactive({ awesomeIcons(
-  icon = 'plus-sign',
-  iconColor = 'white',
-  library = 'glyphicon',
-  markerColor = getColor(postcode_api_month())
-)})
-
 ## Leaflet
-
-
-postcodemap <- reactive({
-  if (input$selectyear01 == "2015")
-  {
+## Base leaflet map
+output$postcodemap <- renderLeaflet({
+  withProgress(message = 'Data is loading, please wait ...', value = 1:100, {
     leaflet() %>%
       addTiles() %>%
+      setView(-2.3590, 51.3811, zoom = 10) %>%
       addProviderTiles("OpenStreetMap", group = "Street map") %>%
       addProviderTiles("Esri.WorldImagery", group = "Satellite") %>%
-      setView(lng=sub_coord()$V1, lat=sub_coord()$V2 , zoom=11) %>%
       addMiniMap(position = "bottomright") %>%
       addLayersControl(
-        position = "topright",
-        baseGroups = c("Street map","Satellite"), 
-        overlayGroups = c("CCG",paste(input$selected_api))
-      ) %>%
-      addPolygons( 
-        data = sub_region(),
-        layerId = ~ccg15nm,
-        color = "orange",
-        weight = 4,
-        fill = FALSE,
-        group = "CCG",
-        # Highlight regions upon mouseover
-        highlight = highlightOptions(
-          weight = 5,
-          fillOpacity = 0,
-          color = "orange",
-          opacity = 1.0,
-          bringToFront = TRUE,
-          sendToBack = TRUE)                            
-      ) %>% 
-      addAwesomeMarkers (
-        data = postcode_api_month(),
-        lng = ~longitude,
-        lat = ~latitude,
-        layerId = ~postcode,
-        group = paste(input$selected_api),
-        icon=icons(),
-        label=~ paste0('click to view'),
-        popup = paste(
-          "<center>","<b>",input$selected_api,"</b>","</center>",
-          "<br>",
-          "Postcode:",  postcode_api_month()$postcode,
-          "<br>",
-          "Prescribed quantity (kg):",postcode_api_month()$kg
-        )
-      ) %>%
-      addLegend("topright", 
-                colors =c("green", "orange", "red"),
-                labels= c("< 0.1", "> 0.1 & < 0.4", "> 0.4"),
-                title= "Quantity (kg)",
-                opacity = 1
-      )
-    
-  }
-  else if (input$selectyear01 == "2016")
-  {
-    leaflet() %>%
-      addTiles() %>%
-      addProviderTiles("OpenStreetMap", group = "Street map") %>%
-      addProviderTiles("Esri.WorldImagery", group = "Satellite") %>%
-      setView(lng=sub_coord()$V1, lat=sub_coord()$V2 , zoom=11) %>%
-      addMiniMap(position = "bottomright") %>%
-      addLayersControl(
-        position = "topright",
-        baseGroups = c("Street map","Satellite"), 
-        overlayGroups = c("CCG",paste(input$selected_api))
-      ) %>%
-      addPolygons( 
-        data = sub_region(),
-        layerId = ~ccg16nm,
-        color = "orange",
-        weight = 4,
-        fill = FALSE,
-        group = "CCG",
-        # Highlight regions upon mouseover
-        highlight = highlightOptions(
-          weight = 5,
-          fillOpacity = 0,
-          color = "orange",
-          opacity = 1.0,
-          bringToFront = TRUE,
-          sendToBack = TRUE)                            
-      ) %>% 
-      addAwesomeMarkers (
-        data = postcode_api_month(),
-        lng = ~longitude,
-        lat = ~latitude,
-        layerId = ~postcode,
-        group = paste(input$selected_api),
-        icon=icons(),
-        label=~ paste0('click to view'),
-        popup = paste(
-          "<center>","<b>",input$selected_api,"</b>","</center>",
-          "<br>",
-          "Postcode:",  postcode_api_month()$postcode,
-          "<br>",
-          "Prescribed quantity (kg):",postcode_api_month()$kg
-        )
-      ) %>%
-      addLegend("topright", 
-                colors =c("green", "orange", "red"),
-                labels= c("< 0.1", "> 0.1 & < 0.4", "> 0.4"),
-                title= "Quantity (kg)",
-                opacity = 1
-      )
-  }
-  else if (input$selectyear01 == "2017")
-  {
-    leaflet() %>%
-      addTiles() %>%
-      addProviderTiles("OpenStreetMap", group = "Street map") %>%
-      addProviderTiles("Esri.WorldImagery", group = "Satellite") %>%
-      setView(lng=sub_coord()$V1, lat=sub_coord()$V2 , zoom=11) %>%
-      addMiniMap(position = "bottomright") %>%
-      addLayersControl(
-        position = "topright",
-        baseGroups = c("Street map","Satellite"), 
-        overlayGroups = c("CCG",paste(input$selected_api))
-      ) %>%
-      addPolygons( 
-        data = sub_region(),
-        layerId = ~ccg17nm,
-        color = "orange",
-        weight = 4,
-        fill = FALSE,
-        group = "CCG",
-        # Highlight regions upon mouseover
-        highlight = highlightOptions(
-          weight = 5,
-          fillOpacity = 0,
-          color = "orange",
-          opacity = 1.0,
-          bringToFront = TRUE,
-          sendToBack = TRUE)                            
-      ) %>% 
-      addAwesomeMarkers (
-        data = postcode_api_month(),
-        lng = ~longitude,
-        lat = ~latitude,
-        layerId = ~postcode,
-        group = paste(input$selected_api),
-        icon=icons(),
-        label=~ paste0('click to view'),
-        popup = paste(
-          "<center>","<b>",input$selected_api,"</b>","</center>",
-          "<br>",
-          "Postcode:",  postcode_api_month()$postcode,
-          "<br>",
-          "Prescribed quantity (kg):",postcode_api_month()$kg
-        )
-      ) %>%
-      addLegend("topright", 
-                colors =c("green", "orange", "red"),
-                labels= c("< 0.1", "> 0.1 & < 0.4", "> 0.4"),
-                title= "Quantity (kg)",
-                opacity = 1
-      )
-  }
-  else if (input$selectyear01 == "2018")
-  {
-    leaflet() %>%
-      addTiles() %>%
-      addProviderTiles("OpenStreetMap", group = "Street map") %>%
-      addProviderTiles("Esri.WorldImagery", group = "Satellite") %>%
-      setView(lng=sub_coord()$V1, lat=sub_coord()$V2 , zoom=11) %>%
-      addMiniMap(position = "bottomright") %>%
-      addLayersControl(
-        position = "topright",
-        baseGroups = c("Street map","Satellite"), 
-        overlayGroups = c("CCG",paste(input$selected_api))
-      ) %>%
-      addPolygons( 
-        data = sub_region(),
-        layerId = ~ccg18nm,
-        color = "orange",
-        weight = 4,
-        fill = FALSE,
-        group = "CCG",
-        # Highlight regions upon mouseover
-        highlight = highlightOptions(
-          weight = 5,
-          fillOpacity = 0,
-          color = "orange",
-          opacity = 1.0,
-          bringToFront = TRUE,
-          sendToBack = TRUE)                            
-      ) %>% 
-      addAwesomeMarkers (
-        data = postcode_api_month(),
-        lng = ~longitude,
-        lat = ~latitude,
-        layerId = ~postcode,
-        group = paste(input$selected_api),
-        icon=icons(),
-        label=~ paste0('click to view'),
-        popup = paste(
-          "<center>","<b>",input$selected_api,"</b>","</center>",
-          "Postcode:",  postcode_api_month()$postcode,
-          "<br>",
-          "Prescribed quantity (kg):",postcode_api_month()$kg
-        )
-      ) %>%
-      addLegend("topright", 
-                colors =c("green", "orange", "red"),
-                labels= c("< 0.1", "> 0.1 & < 0.4", "> 0.4"),
-                title= "Quantity (kg)",
-                opacity = 1
-      )
-  }
+                position = "topright",
+                baseGroups = c("Street map","Satellite"),
+                overlayGroups = c("CCG")
+              )
+  })
 })
+
+
+
+
+observeEvent(input$gen_leaflet01, {
+  postcode_api_month <- reactive ({
+    postcode_api() %>%
+      filter (month %in% month_sel())
+  })
+  # # leaflet icons
+  icons <- reactive({ awesomeIcons(
+    icon = 'plus-sign',
+    iconColor = 'white',
+    library = 'glyphicon',
+    markerColor = getColor(postcode_api_month())
+  )})
+
+  leafletProxy("postcodemap") %>%
+   addAwesomeMarkers (
+    data = postcode_api_month(),
+    lng = ~longitude,
+    lat = ~latitude,
+    layerId = ~postcode,
+    group = paste(input$selected_api),
+    icon=icons(),
+    label=~ paste0('click to view'),
+    popup = paste(
+      "<center>","<b>",input$selected_api,"</b>","</center>",
+      "<br>",
+      "Postcode:",  postcode_api_month()$postcode,
+      "<br>",
+      "Prescribed quantity (kg):",postcode_api_month()$kg
+    )
+  )
+})
+
 
 # For lineplot
+observeEvent(input$postcodemap_marker_click, {
+  
 dat_lineplotgp <- reactive ({ 
   postcode_api() %>%
     filter (postcode %in% !!input$postcodemap_marker_click$id)
 })
 
-# For pie chart - Medicinal form
-filt_gp_dform <- reactive({
-  bind_practices() %>%
-    filter (!grepl("Error: Table",CPD)) %>% 
-    mutate(Year=(substr(PERIOD, 1, 4))) %>%
-    filter (Year %in% !!input$selectyear01) %>%
-    mutate(gram2 = as.numeric(gram)) %>%
-    mutate(DForm = as.numeric(DForm)) %>%
-    mutate(CD = DForm) %>% 
-    left_join(dform_desc, by="CD") %>%
-    left_join(gp_postcode_full(), by=c("PRACTICE"="PRACTICE", "PERIOD" = "PERIOD")) %>%
-    group_by(NM, DESC, postcode) %>%
-    summarise(gram_sum = sum(gram2, na.rm = T)) %>%
-    mutate(kg = gram_sum/1000) %>%
-    filter (tolower(NM) %in% !!input$selected_api) %>%
-    filter (postcode %in% !!input$postcodemap_marker_click$id)
+# Plotly lineplot 01 - Period
+output$filt_gp_lineplot1 <- renderPlotly({ 
+  withProgress(message = 'Data is loading, please wait ...', value = 1:100, {
+    a1 <- as.data.frame(dat_lineplotgp())
+    a1$PERIOD <-  as.character(a1$PERIOD)
+    a2 <-  plot_ly(a1, x = ~PERIOD, y = ~kg, color = ~SURGERY_NAME) %>%
+      add_markers(showlegend = TRUE) %>%
+      add_lines(showlegend = FALSE) %>%
+      layout(
+        title =  F,
+        autosize = T,
+        showlegend = TRUE,
+        xaxis = list(title = "Year / Month"),
+        yaxis = list (title = "kg / month"),
+        legend = list(orientation = "h",
+                      x = 0.3, 
+                      y = -0.3)
+      )
+    a2
+    
+  })
+})
 })
 
 # Total by postcode
@@ -349,20 +163,20 @@ tot_postcode <- reactive({
   bind_practices () %>%
     filter (!grepl("Error: Table",CPD)) %>%
     dplyr::mutate(NM = tolower(NM) ) %>% 
-    dplyr::filter (str_detect(NM , paste(!!bnf_group$BNF_CHEMICAL_SUBSTANCE,collapse = '|'))) %>%
+    filter (tolower(NM) %in% !!input$selected_api) %>%
     mutate(Year=(substr(PERIOD, 1, 4))) %>%
-    filter (Year %in% !!input$selectyear01) %>%
     mutate(gram2 = as.numeric(gram)) %>%
     left_join(gp_postcode_full(), by=c("PRACTICE"="PRACTICE", "PERIOD" = "PERIOD")) %>%
     group_by(NM, Year, postcode) %>%
     summarise(gram_sum = sum(gram2, na.rm = T)) %>%
-    mutate(kg = gram_sum/1000) %>%
-    filter (tolower(NM) %in% !!input$selected_api)
+    mutate(kg = gram_sum/1000) 
+   
 })
 
 ## Observe event
 observeEvent(input$postcodemap_marker_click, {
   click2 <- input$postcodemap_marker_click
+  
   if(is.null(click2))
     return()
   text11 <- tot_postcode()[grepl(click2$id,tot_postcode()$postcode),] 
@@ -376,7 +190,6 @@ observeEvent(input$postcodemap_marker_click, {
 })
 
 # Data for download csv
-
 # leaflet data monthwise
 downheatmap_period <- reactive ({
   dcast(as.data.frame(postcode_api()), postcode+NM ~ PERIOD,value.var= "kg") 
@@ -401,7 +214,7 @@ output$selected_api_input <- renderUI({
   withProgress(message = 'Data is loading, please wait ...', value = 1:100, {
     selectizeInput(inputId="selected_api",
                    label="Select BNF Chemical Substance:",
-                   choices=unique(as.data.frame(select_api())),selected =head(unique(as.data.frame(select_api() ))),  multiple = FALSE
+                   choices=unique(bnf_group()$BNF_CHEMICAL_SUBSTANCE),selected =head(unique(bnf_group()$BNF_CHEMICAL_SUBSTANCE)),  multiple = FALSE
     )
   })
 })
@@ -434,57 +247,7 @@ output$txt_line_gp_title <- renderUI({ HTML( paste("Quantity of ",input$selected
 
 output$txt_gp_pie_title <- renderUI({ HTML( paste("Total quantity of ",input$selected_api, "<br/>prescribed at ",input$postcodemap_marker_click$id, "in the<br/>year ",input$selectyear01, " (Group by medicinal form)") ) })
 
-## Plots
-
-# Leaflet 01 - Practice wise - Region
-observeEvent(input$gen_leaflet01, {
-  output$postcodemap <- renderLeaflet({
-    withProgress(message = 'Data is loading, please wait ...', value = 1:100, {
-      postcodemap()
-    })
-  })
-})
-
-# Plotly lineplot 01 - Period
-output$filt_gp_lineplot1 <- renderPlotly({ 
-  withProgress(message = 'Data is loading, please wait ...', value = 1:100, {
-    a1 <- as.data.frame(dat_lineplotgp())
-    a1$PERIOD <-  as.character(a1$PERIOD)
-    a2 <-  plot_ly(a1, x = ~PERIOD, y = ~kg, color = ~SURGERY_NAME) %>%
-      add_markers(showlegend = TRUE) %>%
-      add_lines(showlegend = FALSE) %>%
-      layout(
-        title =  F,
-        autosize = T,
-        showlegend = TRUE,
-        xaxis = list(title = "Year / Month"),
-        yaxis = list (title = "kg / month"),
-        legend = list(orientation = "h",
-                      x = 0.3, 
-                      y = -0.3)
-      )
-    a2
-    
-  })
-})
-
-# Pie Chart 01 - Medicinal form
-output$filt_gp_dform_pie <- renderPlotly({ 
-  withProgress(message = 'Data is loading, please wait ...', value = 1:100, {
-    plot_ly(as.data.frame(filt_gp_dform()), labels = ~DESC, values = ~kg,  type = "pie", 
-            textposition = 'inside',
-            hoverinfo = 'label+text'
-    ) %>%
-      layout(title =FALSE,
-             showlegend = TRUE,
-             xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-             yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-             legend = list(orientation = "h"))
-  }) 
-})
-
 ## Download Buttons
-
 # Download html 01
 output$downloadhtml_nt_01 <- downloadHandler(
   filename = function (){ paste(input$selected_api,'_',input$region_select_01,'_',input$selectyear01,'_month_',input$sel_month,'.html',sep = '')},
